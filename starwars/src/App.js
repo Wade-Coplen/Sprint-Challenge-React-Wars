384bf424b3cc689a70384ac99576bd98d00ff3a8
Your Real Name Here <Your Email Address Here>
2019-11-23 13:07:06 -0700

    end of sprint

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+function App() {
+	const [characters, setCharacters] = useState([]); 
+
+	useEffect( () => {
+    axios.get ('https://swapi.co/api/people/')
+    .then(response => {
+      console.log(response);	
+      setCharacters(response.data);
+    })
+    .catch(err => console.log(err))
+    } , [])
+
+
 
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
+    
+      <div className="App"> 
+      
+        <h1 className="Header">React Wars</h1>
+        {chharacters.map(character => {   
+          return 
+            <div>{character.results.name} </div>
+            
+            <h2>{character.results.height} </h2>      
+            <h3>{character.results.mass}</h3>
+            <h3>{character.results.hair_color}</h3>
+            <h3>{character.results.skin_color}</h3>
+          
+        })}           
+      
+            
+    </div>  
+      
+    
   );
 }
 
 export default App;
+
