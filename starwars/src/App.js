ddca33ac9d9c823e396512657e8074a75f8c6ee9
Your Real Name Here <Your Email Address Here>
2019-11-24 01:32:28 -0700

    Commit 2: Attributes from object array are now showing on screen. Unstyled.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,39 +6,29 @@ import axios from 'axios';
 function App() {
 	const [characters, setCharacters] = useState([]); 
 
-	useEffect( () => {
-    axios.get ('https://swapi.co/api/people/')
+	useEffect(() => {
+    axios.get('https://swapi.co/api/people/')
     .then(response => {
       console.log(response);	
-      setCharacters(response.data);
+      setCharacters(response.data.results);      
     })
     .catch(err => console.log(err))
     } , [])
-
-
-
-  return (
-    
-      <div className="App"> 
-      
-        <h1 className="Header">React Wars</h1>
-        {chharacters.map(character => {   
-          return 
-            <div>{character.results.name} </div>
-            
-            <h2>{character.results.height} </h2>      
-            <h3>{character.results.mass}</h3>
-            <h3>{character.results.hair_color}</h3>
-            <h3>{character.results.skin_color}</h3>
-          
-        })}           
-      
-            
-    </div>  
-      
-    
-  );
+     
+    return (
+          <div className="App">
+            <h1 className="Header">React Wars</h1>           
+              {characters.map(character => {
+              return <div className= "characterData">
+                <h2>{character.name}</h2>                
+                  <p className= "height p">Height: {character.height}</p> 
+                  <p className= "mass p">Mass: {character.mass}</p>                   
+                  <p className= "hairColor p">Hair Color: {character.hair_color}</p>     
+                  <p className= "skinColor p">Skin Color: {character.skin_color}</p>      
+              </div>   
+              })}
+          </div>
+    )      
 }
-
 export default App;
 
