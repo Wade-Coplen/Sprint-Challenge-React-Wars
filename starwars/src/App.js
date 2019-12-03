4fc0c13fcfdda6f4594aac1b659273f8ce85ed0b
Your Real Name Here <Your Email Address Here>
2019-12-02 21:01:28 -0700

    Test

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -11,7 +11,8 @@ const Card =  styled.div`
   padding: 0px 75px;
   margin-right: 34px;
   box-shadow: 3px 3px 3px 2px black;
- 
+  background-image: linear-gradient(-90deg, #b3643a, rgba(255,0,0,0) );
+  
 `;
 const headTwo = {
   fontSize: "30px",
@@ -22,6 +23,17 @@ const headOne = {
   
 };
 
+const ReactWars = styled.div`
+  font-size: 2rem;
+`;
+const ParagraphStyle = styled.p`
+font-weight: bold;
+`;
+// const headingTwoDiv = {
+// border: "1px solid black",
+// padding: 0 
+// };
+//--------------------------------------------------------
 function App() {
 	const [characters, setCharacters] = useState([]); 
 
@@ -39,12 +51,12 @@ function App() {
               <h1 style= {headOne} className="Header">React Wars</h1>           
                 {characters.map(character => {
                 return <Card className= "characterData">
-                  <h2 style={headTwo}>{character.name}</h2>               
-                    <p className= "height p">Height: {character.height}</p> 
-                    <p className= "mass p">Mass: 
-                    {character.mass}</p>                   
-                    <p className= "hairColor p">Hair Color: {character.hair_color}</p>     
-                    <p className= "skinColor p">Skin Color: {character.skin_color}</p>      
+                  <h2 style={headTwo}>{character.name}</h2>             
+                    <ParagraphStyle  className= "height p">Height: {character.height}</ParagraphStyle > 
+                    <ParagraphStyle className= "mass p">Mass: 
+                    {character.mass}</ParagraphStyle>                   
+                    <ParagraphStyle className= "hairColor p">Hair Color: {character.hair_color}</ParagraphStyle>     
+                    <ParagraphStyle className= "skinColor p">Skin Color: {character.skin_color}</ParagraphStyle>      
                   </Card>   
                 })}
             </div>
